e53faa7762b2a909c6734cf3cf6ab551d9583af5
Sergey <39445974+DNTXXX@users.noreply.github.com>
2024-01-28 22:07:52 +0600

    Add files via upload

---
+++ PowerOn.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set WORKING_DIRECTORY="%~dp0
+set host1=192.168.1.102:8081
+set deviceid=1001831cff
+
+@echo off
+if %1==61 set host=%host1%
+
+%WORKING_DIRECTORY%SonoffPowerManager.exe" %deviceid% %host% 1
